9d352331b7ef29f72f35ba180f829bd7418814e6
FritzsHero <fritzshero@gmail.com>
2023-03-11 18:34:07 +0100

    Refactored AddGrassSeed

--- src/main/java/com/pam/harvestcraft/item/PamCropSeedDropRegistry.java
+++ src/main/java/com/pam/harvestcraft/item/PamCropSeedDropRegistry.java
@@ -1,5 +1,6 @@
 package com.pam.harvestcraft.item;
 
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -13,68 +14,15 @@ public class PamCropSeedDropRegistry
             return;
         }
 
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.AgaveSeedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.AmaranthSeedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.ArrowrootSeedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.asparagusseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.barleyseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.beanseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.beetseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.broccoliseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cauliflowerseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.celeryseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cranberryseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.garlicseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.gingerseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.leekseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.lettuceseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.oatsseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.onionseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.parsnipseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.peanutseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.pineappleseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.radishseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.riceseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.rutabagaseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.ryeseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.scallionseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.soybeanseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.spiceleafseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.sweetpotatoseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.teaseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.turnipseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.whitemushroomseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.artichokeseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.bellpepperseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.blackberryseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.blueberryseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.brusselsproutseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cabbageseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cactusfruitseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.candleberryseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cantaloupeseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.chilipepperseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.coffeeseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cornseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cottonseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.cucumberseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.eggplantseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.grapeseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.kiwiseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.mustardseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.okraseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.peasseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.raspberryseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.rhubarbseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.seaweedseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.strawberryseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.tomatoseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.wintersquashseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.zucchiniseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.bambooshootseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.spinachseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.curryleafseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.sesameseedsseedItem, 1, 0), ItemRegistry.seedrarity);
-        MinecraftForge.addGrassSeed(new ItemStack(ItemRegistry.waterchestnutseedItem, 1, 0), ItemRegistry.seedrarity);
+        for(int i = 0; i < ItemRegistry.PamSeeds.length; i++)
+        {
+            AddGrassSeed(ItemRegistry.PamSeeds[i]);
+        }
+    }
+
+
+    private static void AddGrassSeed(Item _item)
+    {
+        MinecraftForge.addGrassSeed(new ItemStack(_item, 1, 0), ItemRegistry.seedrarity);
     }
 }
