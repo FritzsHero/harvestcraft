68283407f5628392907c31b2f7ce70df7ed2d0d5
FritzsHero <fritzshero@gmail.com>
2024-01-27 17:45:16 +0100

    Refactored registration for shaped recipes

--- src/main/java/com/pam/harvestcraft/item/Recipes.java
+++ src/main/java/com/pam/harvestcraft/item/Recipes.java
@@ -689,6 +689,7 @@ public class Recipes
 
      private static void AddRecipe(ItemStack _output, Object[] _input)
      {
+          // In 1.8 the same as GameRegistry.addShapelessRecipe
           recipeList.add(new ShapelessOreRecipe(_output, _input));
      }
 
@@ -902,6 +903,7 @@ public class Recipes
 
      private static void AddSmelting(Item _input, Item _output)
      {
+          // In 1.8 the same as FurnaceRecipes.instance().addSmelting
           GameRegistry.addSmelting(_input, new ItemStack(_output, 1, 0), 0.1F);
      }
 
@@ -912,6 +914,7 @@ public class Recipes
           AddSmelting(ItemRegistry.doughItem, Items.bread);
           AddSmelting(Items.bread, ItemRegistry.toastItem);
           AddSmelting(Items.pumpkin_seeds, ItemRegistry.roastedpumpkinseedsItem);
+          // In 1.8 the same as FurnaceRecipes.instance().addSmeltingRecipeForBlock
           GameRegistry.addSmelting(Blocks.brown_mushroom, new ItemStack(ItemRegistry.grilledmushroomItem, 1, 0), 0.1F);
           GameRegistry.addSmelting(Blocks.red_mushroom, new ItemStack(ItemRegistry.grilledmushroomItem, 1, 0), 0.1F);
 
@@ -969,59 +972,59 @@ public class Recipes
 
      private static void GetShapedRecipes()
 	{
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.potItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.potItem, true, new Object[]{
 	 	 		"X@@", " @@", Character.valueOf('@'), "ingotIron", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.skilletItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.skilletItem, true, new Object[]{
 	 	 		"@  ", " @ ", "  X", Character.valueOf('@'), "ingotIron", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.saucepanItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.saucepanItem, true, new Object[]{
 	 	 		"@ ", "X ", Character.valueOf('@'), "ingotIron", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.bakewareItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.bakewareItem, true, new Object[]{
 	 	 		"@@@", "@ @", "@@@", Character.valueOf('@'), "ingotBrick"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.cuttingboardItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.cuttingboardItem, true, new Object[]{
 	 	 		"@  ", " X ", "  O", Character.valueOf('@'), "ingotIron", Character.valueOf('X'), "stickWood", Character.valueOf('O'), "plankWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.mortarandpestleItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.mortarandpestleItem, true, new Object[]{
 	 	 		"X@X", " X ", Character.valueOf('@'), "stickWood", Character.valueOf('X'), "stone"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.mixingbowlItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.mixingbowlItem, true, new Object[]{
 	 	 		"X@X", " X ", Character.valueOf('@'), "stickWood", Character.valueOf('X'), "plankWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.juicerItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.juicerItem, true, new Object[]{
 	 	 		"@ ", "X ", Character.valueOf('@'), "stone", Character.valueOf('X'), Blocks.stone_pressure_plate}));
 		
 		//Copper Tools
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.potItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.potItem, true, new Object[]{
 	 	 		"X@@", " @@", Character.valueOf('@'), "ingotCopper", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.skilletItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.skilletItem, true, new Object[]{
 	 	 		"@  ", " @ ", "  X", Character.valueOf('@'), "ingotCopper", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.saucepanItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.saucepanItem, true, new Object[]{
 	 	 		"@  ", "X  ", Character.valueOf('@'), "ingotCopper", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.cuttingboardItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.cuttingboardItem, true, new Object[]{
 	 	 		"@  ", " X ", "  O", Character.valueOf('@'), "ingotCopper", Character.valueOf('X'), "stickWood", Character.valueOf('O'), "plankWood"}));
 		//Steel Tools
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.potItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.potItem, true, new Object[]{
 	 	 		"X@@", " @@", Character.valueOf('@'), "ingotSteel", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.skilletItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.skilletItem, true, new Object[]{
 	 	 		"@  ", " @ ", "  X", Character.valueOf('@'), "ingotSteel", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.saucepanItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.saucepanItem, true, new Object[]{
 	 	 		"@  ", "X  ", Character.valueOf('@'), "ingotSteel", Character.valueOf('X'), "stickWood"}));
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.cuttingboardItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.cuttingboardItem, true, new Object[]{
 	 	 		"@  ", " X ", "  O", Character.valueOf('@'), "ingotSteel", Character.valueOf('X'), "stickWood", Character.valueOf('O'), "plankWood"}));
 		//Nether Brick Bakeware
-		CraftingManager.getInstance().getRecipeList().add(new ShapedOreRecipe(ItemRegistry.bakewareItem, true, new Object[]{
+		recipeList.add(new ShapedOreRecipe(ItemRegistry.bakewareItem, true, new Object[]{
 	 	 		"@@@", "@ @", "@@@", Character.valueOf('@'), "ingotBrickNether"}));
 		
 		//Market Recipe
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(BlockRegistry.pamMarket, 1), new Object[]
-            {
-                "XOX", "OEO", "XOX",'X', "plankWood", 'O', Blocks.wool, 'E', Items.emerald
-            }));
+               {
+                    "XOX", "OEO", "XOX",'X', "plankWood", 'O', Blocks.wool, 'E', Items.emerald
+               }));
 		
 		//Cotton Seed
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(ItemRegistry.cottonseedItem, ItemRegistry.cottonItem));
+		recipeList.add(new ShapelessOreRecipe(ItemRegistry.cottonseedItem, ItemRegistry.cottonItem));
 		//Woven Cloth Recipes
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(new ItemStack(Items.string, 2), "cropCotton", "cropCotton", "cropCotton"));
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(new ItemStack(ItemRegistry.wovencottonItem, 1), Items.string, Items.string));
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(new ItemStack(ItemRegistry.wovencottonItem, 3), "materialCloth", "materialCloth", "materialCloth"));
+		recipeList.add(new ShapelessOreRecipe(new ItemStack(Items.string, 2), "cropCotton", "cropCotton", "cropCotton"));
+		recipeList.add(new ShapelessOreRecipe(new ItemStack(ItemRegistry.wovencottonItem, 1), Items.string, Items.string));
+		recipeList.add(new ShapelessOreRecipe(new ItemStack(ItemRegistry.wovencottonItem, 3), "materialCloth", "materialCloth", "materialCloth"));
 		//Woven Cloth into Wool Recipe
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(new ItemStack(Blocks.wool, 1, 0), "materialCloth", "materialCloth"));
+		recipeList.add(new ShapelessOreRecipe(new ItemStack(Blocks.wool, 1, 0), "materialCloth", "materialCloth"));
 				
 		//Cotton Armor Recipes
 		GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(Items.leather_helmet, 1), new Object[]
@@ -1041,13 +1044,13 @@ public class Recipes
 		        "X X", "X X", 'X', ItemRegistry.wovencottonItem
 		    }));
 		        
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(ItemRegistry.candleberryseedItem, ItemRegistry.candleberryItem));
+		recipeList.add(new ShapelessOreRecipe(ItemRegistry.candleberryseedItem, ItemRegistry.candleberryItem));
 		      
 		//Random Recipes
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(new ItemStack(Items.slime_ball, 1), "foodJellyfishraw"));
+		recipeList.add(new ShapelessOreRecipe(new ItemStack(Items.slime_ball, 1), "foodJellyfishraw"));
 				
 		//Pumpkin Lantern
-		CraftingManager.getInstance().getRecipeList().add(new ShapelessOreRecipe(new ItemStack(Blocks.lit_pumpkin, 1), "cropPumpkin", "blockTorch"));
+		recipeList.add(new ShapelessOreRecipe(new ItemStack(Blocks.lit_pumpkin, 1), "cropPumpkin", "blockTorch"));
 
 		//Logs
 		GameRegistry.addShapedRecipe(new ItemStack(Blocks.planks, 4, 1), "L", 'L', new ItemStack(BlockRegistry.pamMaple));
