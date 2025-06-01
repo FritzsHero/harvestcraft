5cb02ab301afedf11e868bc3c954e8f1be686312
FritzsHero <fritzshero@gmail.com>
2025-06-01 11:42:27 +0200

    Added candleberry to soggyGarden drops

--- src/main/java/com/pam/harvestcraft/Config.java
+++ src/main/java/com/pam/harvestcraft/Config.java
@@ -66,7 +66,9 @@ public class Config
 						"harvestcraft:asparagusItem", "harvestcraft:cranberryItem", "harvestcraft:riceItem",
 						"harvestcraft:seaweedItem", "harvestcraft:waterchestnutItem", "harvestcraft:okraItem",
 						"harvestcraft:GreenGrapeItem", "harvestcraft:JicamaItem", "harvestcraft:KaleItem",
-						"harvestcraft:MilletItem", "harvestcraft:MulberryItem", "harvestcraft:TomatilloItem"},
+						"harvestcraft:MilletItem", "harvestcraft:MulberryItem", "harvestcraft:TomatilloItem",
+						"harvestcraft:candleberryItem"
+				},
 				"comment"));
 		dropConfig.put("tropicalGarden", config.getStringList("tropicalGarden", "drops",
 				new String[]{"harvestcraft:grapeItem", "harvestcraft:pineappleItem", "harvestcraft:kiwiItem",
